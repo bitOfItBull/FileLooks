16727dcd497007d60f394d0d0f02e0700d0573e7
bitOfItBull <80208596+bitOfItBull@users.noreply.github.com>
2023-11-06 07:52:00 +0800

    load folder

---
+++ FileLooks/Model/Folder.cs
@@ -0,0 +1,19 @@
+﻿
+using CommonLib.Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileLooks.Model
+{
+    public class Folder : NotifyBase
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public ObservableCollection<Folder> SubFolders { get; set; }
+
+    }
+}
--- FileLooks/ViewModel/MainWindowViewModel.cs
+++ FileLooks/ViewModel/MainWindowViewModel.cs
@@ -1,12 +1,76 @@
-﻿using System;
+﻿using CommonLib.Common;
+using FileLooks.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace FileLooks.ViewModel
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : NotifyBase
     {
+
+
+        private string select_path;
+
+        public string SelectPath
+        {
+            get { return select_path; }
+            set
+            {
+                select_path = value;
+                DoNotifyPropertyChanged();
+            }
+        }
+
+
+        public ObservableCollection<Folder> Folders { get; set; }
+
+
+
+        public CommandBase Cmd_SelectPath { get; set; }
+
+
+        public MainWindowViewModel()
+        {
+
+            Cmd_SelectPath = new CommandBase(Click_SelectPath);
+
+            Folders = new ObservableCollection<Folder>();
+        }
+
+        private void Click_SelectPath(object o)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            DialogResult result = folderBrowserDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                string selectedPath = folderBrowserDialog.SelectedPath;
+                SelectPath = selectedPath;
+
+
+                Folder folder = new Folder();
+                folder.Path = selectedPath;
+                folder.Name = Path.GetFileName(selectedPath);
+                folder.SubFolders = new ObservableCollection<Folder> { };
+                Folders.Add(folder);
+
+                LoadDirectories(SelectPath, folder);
+            }
+        }
+
+
+        public void LoadDirectories(string path, Folder parent)
+        {
+            
+        }
+
+
     }
 }
