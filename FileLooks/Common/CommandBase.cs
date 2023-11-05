eeccc4fd43a55ad45a8ab5d6b6244766cb0dd947
veryn <veryniubi01@gmail.com>
2023-11-05 23:49:13 +0800

    add common lib

---
+++ FileLooks/Common/CommandBase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CommonLib.Common
+{
+    public class CommandBase : ICommand
+    {
+        public CommandBase() { }
+
+        public CommandBase(Action<object> DoExecute)
+        {
+            this.DoExecute = DoExecute;
+        }
+
+        public CommandBase(Action<object> DoExecute, Func<object, bool> DoCanExecute)
+        {
+            this.DoExecute = DoExecute;
+            this.DoCanExecute = DoCanExecute;
+        }
+
+        //  public event EventHandler CanExecuteChanged;
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return DoCanExecute == null ? true : DoCanExecute.Invoke(parameter);
+            //return DoCanExecute?.Invoke(parameter)==true;
+        }
+
+        public void Execute(object parameter)
+        {
+            DoExecute?.Invoke(parameter);
+        }
+
+
+        public Action<object> DoExecute { get; set; }
+
+        public Func<object, bool> DoCanExecute { get; set; }
+
+    }
+}
---
+++ FileLooks/Common/NotifyBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CommonLib.Common
+{
+    public class NotifyBase : INotifyPropertyChanged
+    {
+
+        public delegate void OnRusultCallback(NotifyBase notify);
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void DoNotifyPropertyChanged([CallerMemberName] string propName = "")
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
